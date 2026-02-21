29c5d7d880c79c4d00c29a5386223793b4f158fd
unknown <aleenaterasa@gmail.com>
2026-02-21 19:00:47 +0530

    trailing space gone

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,13 +3,19 @@ public class Triangle {
     public static void main(String[] args) {
 
         int n = 5;
-        String s = "* ";
 
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) {
-                System.out.println(s.repeat(i));
+
+                for (int j = 1; j <= i; j++) {
+                    if (j == i)
+                        System.out.print("*");
+                    else
+                        System.out.print("* ");
+                }
+
+                System.out.println();
             }
         }
     }
 }
-        
