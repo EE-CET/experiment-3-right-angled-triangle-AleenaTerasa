6d803fd031858c965f9dbf0e405490c4aa4c190e
unknown <aleenaterasa@gmail.com>
2026-02-21 18:49:44 +0530

    works

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,15 @@
 public class Triangle {
-    //Add your programs here
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        String s = "*";
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(s.repeat(i));
+            }
+        }
+    }
 }
+        
