bbf71be630c15369c5999cc9348447ea38454a60
unknown <aleenaterasa@gmail.com>
2026-02-21 18:53:18 +0530

    spacing between stars

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,7 +3,7 @@ public class Triangle {
     public static void main(String[] args) {
 
         int n = 5;
-        String s = "*";
+        String s = "* ";
 
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) {
